d0a44a695f1cd58cd82a8164378c4f691d32b5f9
Victor Manuel Echavarria <vicme93@gmail.com>
2019-09-13 20:59:43 -0500

    cronometro

---
+++ Cronometro/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cronometro
+{
+    public partial class Form1 : Form
+    {
+
+        int contador=10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncrono_Click(object sender, EventArgs e)
+        {
+            tiempo.Start();
+
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            
+            }
+
+        private void tiempo_Tick(object sender, EventArgs e)
+        {
+            lblcontador.Text = contador.ToString();
+            contador = contador - 1;
+
+            if (contador < 0)
+            {
+
+                tiempo.Stop();
+                MessageBox.Show("Conteo Completo");
+            }
+    }
+    }
+}
